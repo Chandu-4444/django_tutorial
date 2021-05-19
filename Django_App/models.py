4e6c2ff94ab66232571d09cf6e2ce7fcb2dbc7a8
Chandu-4444 <chandrakiran.g19@iiits.in>
2021-05-19 17:35:23 +0530

    Add models

--- Django_App/models.py
+++ Django_App/models.py
@@ -1,8 +1,29 @@
 from django.db import models
 
 # Create your models here.
-class Student(models.Model):
+
+class Author(models.Model):
     name = models.CharField(max_length=200)
+
+
+    def __str__(self):
+        return self.name
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    body = models.CharField(max_length=200)
+    author = models.ManyToManyField(Author, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.name
+
+
+
+
+
+
     
 
 
--- Django_App/views.py
+++ Django_App/views.py
@@ -1,27 +1,26 @@
 from django.shortcuts import render, HttpResponse, redirect, reverse
+from .models import Blog
 
 # Create your views here.
+# results = 0
 
 def home(request):
     if request.method == 'GET':
         return render(request, 'home.html')
     elif request.method == 'POST':
-        entered_name = request.POST['user_name']
-        email = request.POST['email']
-        
-        print(entered_name, email)
-        # return render(request, 'contacts.html')
+        # global results
+        # user_name = request.POST['user_name']
+        # new_obj = Detail()
+        # new_obj.name = user_name
+        # new_obj.save()
+        # results = Detail.objects.filter(name__icontains = search_input)
         return redirect(reverse('contacts'))
 
 
-
-    
-
-
-
 def contacts_page(request):
-    return render(request, 'contacts.html')
-
+    # global results
+    # objects_list = Detail.objects.all()
+    return render(request, 'contacts.html', {'list': objects_list})
 
 
 
